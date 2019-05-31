5891760d460ecf7318f6a8d49a0613161f70565c
Jsaalfeld7 <45191544+Jsaalfeld7@users.noreply.github.com>
2019-05-31 10:26:29 -0700

    Starter Code

---
+++ Conc2.m
@@ -0,0 +1,3 @@
+function [C2] = Conc2(alpha)
+C2 = C0*exp(alpha)*(1-alpha); %concentration at the top of the ascending LH
+end
---
+++ Parameters.m
@@ -0,0 +1,8 @@
+%% Parameters 
+
+Fna = ;
+C0 = ;
+Q0 = ;
+L = ;
+
+alpha = Fna*L/(Q0*C0);
