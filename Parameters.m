f4a6f6228a41552d027935ed81f9dcd517783c99
natalietetreault <46908136+natalietetreault@users.noreply.github.com>
2019-06-03 15:24:28 -0700

    Updated for Arbitrary Units

--- Parameters.m
+++ Parameters.m
@@ -1,12 +1,12 @@
 %% Parameters 
-global alpha L Fna Q1_0 C0
-%notes! no idea if these values are realistic haha i just put something to
-%test the scripts
 
-Fna = 20; %sodium flux in ions/(min*mm)
-C0 = 140; %c(0) in mol/L
-Q1_0 = 2; %Q1(0) in L/min
-L = 10; %nephron length in mm
-C_star= 60; %desired urine concentration for feedback mechanism
+%note: values are NOT realistic and units are arbitrary
+
+Fna = 20; %sodium flux in mol/(time*length)
+C0 = 140; %c(0) in mol/volume
+Q1_0 = 2; %Q1(0) in volume/time
+L = 10; %nephron length
+C_star = 60; %desired urine concentration for feedback mechanism
 
 alpha = Fna*L/(Q1_0*C0);
+alpha_check=(alpha<=1) %returns true if alpha is less than or equal to 1
