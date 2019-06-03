de25d63d0d71bbc6f5d4a318b43b0c25dd3ca731
natalietetreault <46908136+natalietetreault@users.noreply.github.com>
2019-06-03 14:17:15 -0700

    Updated Feedback Section

--- nephron_model.m
+++ nephron_model.m
@@ -5,18 +5,7 @@ global C0 Fna Q1_0 alpha C_star L;
 x=0:0.1:L;
 
 C1=conc1(x);
-<<<<<<< HEAD
-figure(1)
-plot(x,C1);
-xlabel('x (mm)','FontSize',16); ylabel('[Na]','FontSize',16); title('Descending Loop of Henle Concentration','FontSize',16)
-
-C2=Conc2(x,L);
-figure(2)
-plot(x,C2);
-xlabel('x (mm)','FontSize',16); ylabel('[Na]','FontSize',16); title('Ascending Loop of Henle Concentration','FontSize',16)
-=======
-C2=Conc2(x,L);
->>>>>>> ddaac14176efb4bec2eea82e0823e11f343295ac
+C2=Conc2(x);
 
 figure(1)
 plot([C1 flip(C2)])
@@ -38,9 +27,10 @@ ADH=1; %indicate if ADH is present or not
 %or maybe write some function/expression to choose base on C0?
 
 %print out useful info:
-a=exp(alpha)*(1-alpha)
 Q1_0=Fna*L/C0
+alpha=Fna*L/(Q1_0*C0);
 CL=exp(1)*C0
+a=exp(alpha)*(1-alpha)
 
 Q3=Fna*L*C_star/(exp(1)*C0)^2;
 Q2=Fna*L/(exp(1)*C0);
@@ -66,30 +56,12 @@ xlabel('Minutes','FontSize',16); ylabel('Na excreted (mol)','FontSize',16); titl
 hold on
 plot(t,Qalt.*t*C_star,'--'); legend('Actual','Alternative Choice');
 
-figure(3)
 C1=conc1(x);
+C2=(exp(1)*C0-C_star)/L*x+C_star;
+
 figure(3)
-plot(x,C1);
-xlabel('x (mm)','FontSize',16); ylabel('[Na]','FontSize',16); title('Descending Loop of Henle Concentration','FontSize',16)
-
+plot([C1 flip(C2)])
+ylabel('[Na]','FontSize',16); title('Concentration Through Loop of Henle','FontSize',16);
+xticks([0 101 202]);
+xticklabels({'0','L','0'});
 %% With Varying Nephron Lengths
-
-for L = 1:5                %vary nephron lengths of individual nephrons (mm)
-    Q1_0 = Fna*L/C0;        %find initial flow     
-    Q2 = Fna*L/(exp(1)*C0); %find final flow
-    x=0:0.1:L;              %vary x along L
-    C1 = conc1(x);          %find descending concentration
-    C2 = Conc2(x,L);        %find ascending concentration
-    
-    figure(7)               %plot concentration vs distance for each nephron length
-    plot(x,C1)
-    hold on
-    figure(8)
-    plot(x,C2)
-    hold on
-end
-figure(7)
-xlabel('x (mm)','FontSize',16); ylabel('[Na]','FontSize',16); title('Descending Loop of Henle Concentration','FontSize',16)
-figure(8)
-xlabel('x (mm)','FontSize',16); ylabel('[Na]','FontSize',16); title('Ascending Loop of Henle Concentration','FontSize',16)
-hold off
