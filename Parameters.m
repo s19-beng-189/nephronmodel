463a3e22d0e929361266e51f0e49140d572c4362
natalietetreault <46908136+natalietetreault@users.noreply.github.com>
2019-06-04 12:52:02 -0700

    Starting Values
    Values to be used for relative comparison while running modified scenarios

--- Parameters.m
+++ Parameters.m
@@ -2,11 +2,11 @@
 
 %note: values are NOT realistic and units are arbitrary
 
-Fna = 20; %sodium flux in mol/(time*length)
-C0 = 140; %c(0) in mol/volume
+Fna = 25; %sodium flux in mol/(time*length)
+C0 = 150; %c(0) in mol/volume
 Q1_0 = 2; %Q1(0) in volume/time
 L = 10; %nephron length
-C_star = 60; %desired urine concentration for feedback mechanism
+C_star = 50; %desired urine concentration for feedback mechanism
 
 alpha = Fna*L/(Q1_0*C0);
 alpha_check=(alpha<=1) %returns true if alpha is less than or equal to 1
